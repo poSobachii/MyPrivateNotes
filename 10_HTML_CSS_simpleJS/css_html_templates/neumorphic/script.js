3cb0a215287844c16917994c2e34705537b2ed6a
Aleksandr Grishanov <Aleksandr.Grishanov@swisscom.com>
2022-10-17 16:41:07 +0300

    emergency commit

--- 10_HTML_CSS_simpleJS/css_html_templates/neumorphic/script.js
+++ 10_HTML_CSS_simpleJS/css_html_templates/neumorphic/script.js
@@ -1,10 +1,81 @@
-// window.onload=function(){
-//     alert('Alert on load');
+
+/* Snippet ===============================================================================================================================*/
+
+const pulseCircle = document.querySelector('.pulsing-circle');
+const wave1 = document.querySelector('.pulsing-circle-wave');
+const wave2 = document.querySelector('.offset-wave-1');
+
+pulseCircle.addEventListener('click', function(e) {
+    e.preventDefault();
+    pulseCircle.classList.toggle('paused');
+    wave1.classList.toggle('paused');
+    wave2.classList.toggle('paused');
+});
+
+/* Snippet ===============================================================================================================================*/
+
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+function clock(){
+    let today = new Date();
+    let h = (today.getHours() % 12) + today.getMinutes() / 59; // 22 % 12 = 10pm
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+
+    // let m = today.getMinutes() + today.getSeconds() / 59;
+    // let s = today.getSeconds() + today.getMilliseconds() / 1000;         // smooth minutes, seconds
+
+    h *= 30; // 12 * 30 = 360deg
+    m *= 6;
+    s *= 6; // 60 * 6 = 360deg
+
+    rotation(hours, h);
+    rotation(minutes, m);
+    rotation(seconds, s);
+
+    setTimeout(clock, 500);
+}
+
+function rotation(target, val){
+    target.style.transform =  `rotate(${val}deg)`;
+}
+
+window.onload = clock();
+
+
+// const hours = document.querySelector('.hours');
+// const minutes = document.querySelector('.minutes');
+// const seconds = document.querySelector('.seconds');
+//
+// clock = () => {
+//     let today = new Date();
+//     let h = (today.getHours() % 12) + today.getMinutes() / 59; // 22 % 12 = 10pm
+//     let m = today.getMinutes();
+//     let s = today.getSeconds();
+//
+//     // let m = today.getMinutes() + today.getSeconds() / 59;
+//     // let s = today.getSeconds() + today.getMilliseconds() / 1000;         // smooth minutes, seconds
+//
+//     h *= 30; // 12 * 30 = 360deg
+//     m *= 6;
+//     s *= 6; // 60 * 6 = 360deg
+//
+//     rotation(hours, h);
+//     rotation(minutes, m);
+//     rotation(seconds, s);
+//
+//     setTimeout(clock, 500);
 // }
+//
+// rotation = (target, val) => {
+//     target.style.transform =  `rotate(${val}deg)`;
+// }
+//
+// window.onload = clock();
 
 
-/* Snippet ===============================================================================================================================*/
-/* Snippet ===============================================================================================================================*/
 /* Snippet ===============================================================================================================================*/
 /* Snippet ===============================================================================================================================*/
 /* Snippet ===============================================================================================================================*/
